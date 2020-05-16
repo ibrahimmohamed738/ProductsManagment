583118d09e4077b97ff81e7659ddca787e13890c
ibrahim <ibrahim@IBRLTP01>
2020-05-17 00:57:29 +0300

    Adding Project to repo

---
+++ ProductMangmentSystem/DAL/DataAccessLayer.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace ProductMangmentSystem.DAL
+{
+    class DataAccessLayer
+    {
+        SqlConnection sqlconnection;
+        public DataAccessLayer()
+        {
+            sqlconnection = new SqlConnection(@"Server=.\SQLEXPRESS;Database=ProductDB;User=sa;Pwd=0328655@joker");
+        }
+
+        public void Open() 
+        {
+            if (sqlconnection.State != ConnectionState.Open)
+            {
+                sqlconnection.Open();
+            
+            }
+        
+        }
+
+        public void Close() 
+        {
+            if (sqlconnection.State == ConnectionState.Open)
+            {
+                sqlconnection.Close();
+            
+            }
+        
+        }
+
+        //Method to read from Database
+        public DataTable SelectData(string stored_precedure, SqlParameter[] param)
+        {
+            SqlCommand sqlcmd = new SqlCommand();
+            sqlcmd.CommandType = CommandType.StoredProcedure;
+            sqlcmd.CommandText = stored_precedure;
+
+            if (param != null)
+            {
+                for (int i = 0; i < param.Length; i++)
+                {
+                    sqlcmd.Parameters.Add(param[i]);
+                }
+            
+            }
+            SqlDataAdapter da = new SqlDataAdapter(sqlcmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            return dt;
+        
+        }
+
+        //Method to insert , update and delete data from Database
+        public void ExcuteCommand(string stored_precedure, SqlParameter[] param) 
+        {
+            SqlCommand sqlcmd = new SqlCommand();
+            sqlcmd.CommandType = CommandType.StoredProcedure;
+            sqlcmd.CommandText = stored_precedure;
+            if (param != null)
+            {
+                sqlcmd.Parameters.AddRange(param);
+            }
+
+            sqlcmd.ExecuteNonQuery();
+        
+        }
+    }
+}
